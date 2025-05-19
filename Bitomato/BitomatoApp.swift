700792b9ed8f7cd3269a3b8b37ecb2599cd6eaf8
EugeneSt <eugene.stepchenko@gmail.com>
2025-05-19 09:49:48 +0300

    Initial Commit

---
+++ Bitomato/BitomatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitomatoApp.swift
+//  Bitomato
+//
+//  Created by Eugene St on 19.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BitomatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bitomato/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bitomato
+//
+//  Created by Eugene St on 19.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
